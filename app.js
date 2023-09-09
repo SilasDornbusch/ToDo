deda2c8e2ac29ba2949173e31298ac84ba3cf08e
Silas ツ <silasdornbusch@outlook.de>
2023-09-09 19:36:20 +0200

    ToDo
    ToDo

---
+++ app.js
@@ -0,0 +1,58 @@
+// app.js
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addButton = document.getElementById("addButton");
+    const deleteButton = document.getElementById("deleteButton");
+    const taskList = document.getElementById("taskList");
+
+    addButton.addEventListener("click", addTask);
+    deleteButton.addEventListener("click", deleteTask);
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        if (taskText) {
+            const listItem = document.createElement("li");
+            listItem.textContent = taskText;
+            taskList.appendChild(listItem);
+            taskInput.value = "";
+            saveTasks();
+        }
+    }
+
+    function deleteTask() {
+        const selectedTask = taskList.querySelector("li.selected");
+        if (selectedTask) {
+            taskList.removeChild(selectedTask);
+            saveTasks();
+        } else {
+            alert("Bitte eine Aufgabe auswählen!");
+        }
+    }
+
+    function saveTasks() {
+        const tasks = [];
+        taskList.querySelectorAll("li").forEach((task) => {
+            tasks.push(task.textContent);
+        });
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function loadTasks() {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.forEach((taskText) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = taskText;
+            listItem.addEventListener("click", selectTask);
+            taskList.appendChild(listItem);
+        });
+    }
+
+    function selectTask(event) {
+        taskList.querySelectorAll("li").forEach((task) => {
+            task.classList.remove("selected");
+        });
+        event.target.classList.add("selected");
+    }
+
+    loadTasks();
+});
